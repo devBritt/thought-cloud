18503945ca004182b90e2232ced79041d1248341
Britt Carlton <brittanyrc95@gmail.com>
2022-09-15 12:31:51 -0400

    Create user-controllers skeleton

---
+++ controllers/user-controllers.js
@@ -0,0 +1,20 @@
+const { User } = require('../models');
+
+// crreate user controller methods
+module.exports = {
+    async getAllUsers() {
+        
+    },
+    async getUserById({ params }, res) {
+
+    },
+    async addUser({ body }, res) {
+
+    },
+    async updateUser({ params, body }, res) {
+
+    },
+    async removeUser({ params }, res) {
+
+    }
+};

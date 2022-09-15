127bab3aa94fddcfaa570dc8709e5c6f766e2d50
Britt Carlton <brittanyrc95@gmail.com>
2022-09-15 12:49:15 -0400

    Create api/index.js, routes/index.js and routers

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -3,7 +3,21 @@ const { User } = require('../models');
 // crreate user controller methods
 module.exports = {
     async getAllUsers() {
-        
+        try {
+            // query db for all Users
+            // TODO: add thoughts to populate
+            const userData = await User.find({})
+                .populate({
+                    path: 'friends',
+                    select: '-__v'
+                })
+                .select('-__v');
+
+            console.log(userData);
+        } catch(err) {
+            console.log(err);
+            res.status(400).json(err);
+        }
     },
     async getUserById({ params }, res) {
 
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(require('./routes'));
+app.use(require('./routes'));
 
 // connect mongoose to db
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/thought-cloud', {

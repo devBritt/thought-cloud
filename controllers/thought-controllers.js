90d236af9bab6d682e543f1f9684cda80ec3df89
Britt Carlton <brittanyrc95@gmail.com>
2022-09-19 21:23:17 -0400

    Create getAllThoughts, addThought methods

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -3,13 +3,30 @@ const { Thought } = require('../models');
 // create thought controller methods
 module.exports = {
     async getAllThoughts(req, res) {
-        console.log('In getAllThoughts');
+        try {
+            // query db for all thoughts
+            // TODO: add reactions to populate
+            const userData = await Thought.find({})
+                .select('-__v');
+        res.json(userData);
+        } catch(err) {
+            console.log(err);
+            res.status(400).json(err);
+        }
     },
     async getThoughtById({ params }, res) {
         console.log(params);
     },
     async addThought({ body }, res) {
-        console.log(body);
+        try {
+            // query db to create new user
+            const userData = await Thought.create(body);
+
+            res.json(userData);
+        } catch(err) {
+            console.log(err);
+            res.status(400).json(err);
+        }
     },
     async updateThought({ params, body }, res) {
         console.log(params, body);

cb71c0598855c58b67766d6549e99524de527951
Britt Carlton <brittanyrc95@gmail.com>
2022-09-19 21:33:01 -0400

    Create getThoughtById, change userData to thoughtData

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -6,23 +6,37 @@ module.exports = {
         try {
             // query db for all thoughts
             // TODO: add reactions to populate
-            const userData = await Thought.find({})
+            const thoughtData = await Thought.find({})
                 .select('-__v');
-        res.json(userData);
+        res.json(thoughtData);
         } catch(err) {
             console.log(err);
             res.status(400).json(err);
         }
     },
     async getThoughtById({ params }, res) {
-        console.log(params);
+        try {
+            const thoughtData = await Thought.findById(params.thoughtId)
+                .select('-__v');
+
+            if (thoughtData.length < 1) {
+                console.log("Hmmm... We couldn't find a thought with that ID.");
+                res.status(404).json({ message: "Hmmm... We couldn't find a user with that ID." });
+                return;
+            }
+            
+            res.json(thoughtData);
+        } catch(err) {
+            console.log(err);
+            res.status(400).json(err);
+        }
     },
     async addThought({ body }, res) {
         try {
             // query db to create new user
-            const userData = await Thought.create(body);
+            const thoughtData = await Thought.create(body);
 
-            res.json(userData);
+            res.json(thoughtData);
         } catch(err) {
             console.log(err);
             res.status(400).json(err);
